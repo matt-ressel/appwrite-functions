85a7e7fe9de38a79de74c05738a444030f8f02c4
matt-ressel <mat.ressel@icloud.com>
2024-04-07 20:44:59 +0200

    create: check-user-identify

---
+++ check-user-identify/main.js
@@ -0,0 +1,27 @@
+import { Client, Users, Query } from "node-appwrite";
+
+export default async ({ req, res, log, error }) => {
+  const client = new Client()
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
+
+  const users = new Users(client);
+
+  if (req.method == "POST") {
+    const { type, identifier } = JSON.parse(req.body);
+    // check if user exist with provided identifier
+    const response = await users.list([Query.equal(type, [identifier])]);
+    // show response in function Executions
+    log(response); // res
+    log(error); // error
+    // if response.total > 0 then identifier exist
+    if (response.total) {
+      return res.json({ value: true });
+    } else {
+      return res.json({ value: false });
+    }
+  }
+
+  return res.empty();
+};
